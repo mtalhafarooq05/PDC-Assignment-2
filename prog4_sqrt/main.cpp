ad85e9692683f28221879ef917bdcd444141418c
talha <m.talhafarooq05@gmail.com>
2026-09-18 00:31:45 +0500

    Add max-speedup input for Program 4 SIMD test

--- prog4_sqrt/main.cpp
+++ prog4_sqrt/main.cpp
@@ -34,7 +34,7 @@ int main() {
         // to you generate best and worse-case speedups
         
         // starter code populates array with random input values
-        values[i] = .001f + 2.998f * static_cast<float>(rand()) / RAND_MAX;
+        values[i] = values[i] = 2.999f;
     }
 
     // generate a gold version to check results
